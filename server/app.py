926b8668f6b36374ae4766f1b04eb47f822ff5a4
Densel <densel.esekon@moringaschool.com>
2024-03-09 23:45:22 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -19,20 +19,24 @@ def index():
     return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
-def bakeries():
-    return ''
+def get_bakeries():
+    bakeries = Bakery.query.all()
+    return jsonify([bakery.to_dict() for bakery in bakeries])
 
 @app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
+def get_bakery_by_id(id):
+    bakery = Bakery.query.get(id)
+    return jsonify(bakery.to_dict(include_relationships=True))
 
 @app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    return ''
+def get_baked_goods_by_price():
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return jsonify([baked_good.to_dict() for baked_good in baked_goods])
 
 @app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    return ''
+def get_most_expensive_baked_good():
+    most_expensive_baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return jsonify(most_expensive_baked_good.to_dict())
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
